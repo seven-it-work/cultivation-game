3b38492ac5195878a548e6ecd8268cf7d3cb8327
seven <sevenyjl@gmail.com>
2023-10-05 16:00:04 +0800

    样式去除

--- web-vite/src/App.tsx
+++ web-vite/src/App.tsx
@@ -2,13 +2,13 @@ import React, {useState} from 'react'
 import './App.css'
 import {Link} from "react-router-dom";
 import Router from "./router/index";
+import NavComponent from "./components/NavComponent";
 
 
 function App() {
     return (
         <>
-            <Link to="/"><h4>Home</h4></Link>
-            <Link to="/view1"><h4>View1</h4></Link>
+            <NavComponent/>
             <Router/>
         </>
     )
