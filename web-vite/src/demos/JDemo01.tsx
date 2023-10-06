5ff8bf2fbee6b98ecd445a373900c5e8a8caf24e
seven <sevenyjl@gmail.com>
2023-10-06 15:41:09 +0800

    jsplumb 达到地图连线效果

--- web-vite/src/demos/JDemo01.tsx
+++ web-vite/src/demos/JDemo01.tsx
@@ -1,5 +1,5 @@
 import jsPlumb from "jsplumb";
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 
 export default function JDemo01(props: any) {
     let timer: any;
@@ -27,47 +27,18 @@ export default function JDemo01(props: any) {
             instance.connect({
                 source: "flowChart1",//图一的id
                 target: "flowChart2",//图二的id
-                endpoint: "Blank", //端点的形状设置为空
-                connector: ["Flowchart", {cornerRadius: 2}],
+                endpoint: [  "Dot", { radius:3 } ], //端点的形状设置为空
+                connector: "Straight",
                 anchor: ["Right", "Left"],//连接端点的位置，起始节点的右侧和结束节点的左侧
-                overlays: [
-                    ["Arrow", {width: 12, length: 12, location: m}],//小箭头样式及位置
-                ],
             });
-            //连接图二和图三
+            // //连接图一和图四
             instance.connect({
-                source: "flowChart2",
-                target: "flowChart3",
-                endpoint: "Blank",
-                connector: ["Flowchart", {cornerRadius: 2}],
-                anchor: ["Right", "Left"],
-                overlays: [
-                    ["Arrow", {width: 12, length: 12, location: m}],
-                ],
+                source: "flowChart1",
+                target: "flowChart4",
+                endpoint: [  "Dot", { radius:3 } ], //端点的形状设置为空
+                connector: "Straight",
+                anchor: ["BottomRight", "TopLeft"],
             });
-            //连接图四和图三
-            instance.connect({
-                source: "flowChart4",
-                target: "flowChart3",
-                endpoint: "Blank",
-                connector: ["Flowchart", {cornerRadius: 2}],
-                anchor: ["Right", "Left"],
-                overlays: [
-                    ["Arrow", {width: 12, length: 12, location: m}],
-                ],
-            });
-            //连接图四和图二
-            instance.connect({
-                source: "flowChart4",
-                target: "flowChart2",
-                endpoint: "Blank",
-                connector: ["Flowchart", {cornerRadius: 2}],
-                anchor: ["Bottom", "Bottom"],
-                overlays: [
-                    ["Arrow", {width: 12, length: 12, location: m}],
-                ],
-            });
-
         }, 20);
     };
     useEffect(() => {
@@ -80,11 +51,19 @@ export default function JDemo01(props: any) {
     }, []);
 
     return (
-        <div style={{width: '100%', display: "flex", justifyContent: "space-between"}}>
-            <div id={'flowChart1'} >1</div>
-            <div id={'flowChart2'} >2</div>
-            <div id={'flowChart3'} >3</div>
-            <div id={'flowChart4'} >4</div>
+        <div >
+            <table className={'mapTable'} cellSpacing={20}>
+                <tbody>
+                <tr>
+                    <td id={'flowChart1'}>1</td>
+                    <td  id={'flowChart2'}>2</td>
+                </tr>
+                <tr>
+                    <td id={'flowChart3'}>3</td>
+                    <td id={'flowChart4'}>99994</td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
